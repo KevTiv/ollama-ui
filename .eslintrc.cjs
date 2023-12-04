3d5aafd392c08dd85b3785b95d34e4ba71505d66
Kevin Tivert <tivertc.kevin@outlook.com>
2023-12-04 09:11:06 +0100

    feat: project config setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,11 +4,12 @@ const config = {
   parserOptions: {
     project: true,
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "prettier"],
   extends: [
     "plugin:@next/next/recommended",
     "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:prettier/recommended",
   ],
   rules: {
     // These opinionated rules are enabled in stylistic-type-checked above.
@@ -31,6 +32,7 @@ const config = {
         checksVoidReturn: { attributes: false },
       },
     ],
+    "prettier/prettier": "error",
   },
 };
 
